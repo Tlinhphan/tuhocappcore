f1c86170599ec63d7eea18a7127842df583170a3
Tlinhphan <tlinhphan201@gmail.com>
2025-10-13 01:54:03 +0700

    Test rem

--- TuhocAppCore/Startup.cs
+++ TuhocAppCore/Startup.cs
@@ -39,7 +39,12 @@ namespace TuhocAppCore
             services.AddMvc();
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        /// 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
