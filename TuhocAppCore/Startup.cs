42b11da88367938c4e65d0c50159d55537b96ea2
Tlinhphan <tlinhphan201@gmail.com>
2025-10-13 01:49:25 +0700

    /////

--- TuhocAppCore/Startup.cs
+++ TuhocAppCore/Startup.cs
@@ -39,6 +39,7 @@ namespace TuhocAppCore
             services.AddMvc();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
